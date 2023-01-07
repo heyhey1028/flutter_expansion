ffb68e896fe05c3e09ccf2aaec324ed2f4ad0cf2
heyhey1028 <shoheiogawa1028@gmail.com>
2023-01-07 16:18:05 +0900

    add qiita_search

---
+++ qiita_search/README.md
@@ -0,0 +1,5 @@
+# qiita_search
+
+Qiita を検索する Flutter アプリを作ってみよう
+
+## Getting Started
---
+++ qiita_search/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:qiita_search/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const MyHomePage(title: 'Search Qiita'),
+    );
+  }
+}
