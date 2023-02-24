3bd17cedf719ea763452a589f93430e57f93c81b
heyhey1028 <shoheiogawa1028@gmail.com>
2023-02-24 22:19:15 +0900

    feat: add current location button

--- almost_zenly/lib/screens/map_screen.dart
+++ almost_zenly/lib/screens/map_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
@@ -11,28 +12,54 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  // GoogleMapを操作するためのクラス
   late GoogleMapController mapController;
 
-  // GoogleMap描画時の初期位置
   final CameraPosition initialCameraPosition = const CameraPosition(
-    target: LatLng(35.681236, 139.767125), // 東京駅
+    target: LatLng(35.681236, 139.767125),
     zoom: 16.0,
   );
 
   @override
+  void initState() {
+    //位置情報が許可されていない時に許可をリクエストする
+    Future(() async {
+      LocationPermission permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        await Geolocator.requestPermission();
+      }
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // GoogleMapを表示する
       body: GoogleMap(
         initialCameraPosition: initialCameraPosition,
         onMapCreated: (GoogleMapController controller) {
           mapController = controller;
         },
+        myLocationEnabled: true,
+        myLocationButtonEnabled: false,
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
+        onPressed: () async {
+          // 現在地を取得
+          final Position position = await Geolocator.getCurrentPosition(
+            desiredAccuracy: LocationAccuracy.high,
+          );
+
+          // 現在地を中心にカメラを移動
+          mapController.animateCamera(
+            CameraUpdate.newCameraPosition(
+              CameraPosition(
+                target: LatLng(position.latitude, position.longitude),
+                zoom: 16.0,
+              ),
+            ),
+          );
+        },
+        tooltip: 'current position',
         child: const Icon(Icons.add),
       ),
     );
