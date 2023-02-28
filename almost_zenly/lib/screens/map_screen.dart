27efaaf954aa059180e5d02643a452a9eae8761d
heyhey1028 <shoheiogawa1028@gmail.com>
2023-02-28 13:49:57 +0900

    feat: add location permission

--- almost_zenly/lib/screens/map_screen.dart
+++ almost_zenly/lib/screens/map_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
@@ -11,16 +12,26 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  // GoogleMap描画時の初期位置
   final CameraPosition initialCameraPosition = const CameraPosition(
-    target: LatLng(35.681236, 139.767125), // 東京駅
+    target: LatLng(35.681236, 139.767125),
     zoom: 16.0,
   );
 
   @override
+  void initState() {
+    //位置情報が許可されていない時に許可をリクエストする
+    Future(() async {
+      LocationPermission permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        await Geolocator.requestPermission();
+      }
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // GoogleMapを表示する
       body: GoogleMap(
         initialCameraPosition: initialCameraPosition,
       ),
