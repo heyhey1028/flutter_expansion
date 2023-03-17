9a65c2552faab25cf7e6d9879ce53c17d8bdefbd
heyhey1028 <shoheiogawa1028@gmail.com>
2023-03-17 21:14:05 +0900

    add test_screen for firestore

--- almost_zenly/lib/main.dart
+++ almost_zenly/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:almost_zenly/firebase_options.dart';
-import 'package:almost_zenly/screens/map_screen/map_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -21,7 +21,45 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MapScreen(),
+      home: const MainScreen(),
     );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  List<dynamic> datas = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Firestore test'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(datas.toString()),
+            ElevatedButton(
+              onPressed: onPressed,
+              child: const Text('push'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> onPressed() async {
+    final db = FirebaseFirestore.instance;
+    final collectionRef = db.collection('users');
+    // TODO: CRUD処理を実装していく
   }
 }
