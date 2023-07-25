d06a46d4de6527d7ae5dce823d7dfd339a99c4ad
heyhey1028 <shoheiogawa1028@gmail.com>
2023-07-25 22:50:02 +0900

    feat: add project

---
+++ qiita_search202307/README.md
@@ -0,0 +1,3 @@
+# qiita_search202307
+
+A new Flutter project.
---
+++ qiita_search202307/android/app/src/main/kotlin/com/example/qiita_search202307/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.qiita_search202307
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
