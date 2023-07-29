445601435e10ce5a16a1fa755e1d90d84720a5d7
heyhey1028 <shoheiogawa1028@gmail.com>
2023-07-29 18:00:22 +0900

    Refactor ArticleContainer widget to include an Article object

--- qiita_search202307/lib/widgets/article_container.dart
+++ qiita_search202307/lib/widgets/article_container.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:qiita_search202307/models/article.dart';
 
 class ArticleContainer extends StatelessWidget {
-  const ArticleContainer({super.key});
+  const ArticleContainer({
+    super.key,
+    required this.article,
+  });
+
+  final Article article;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 180, // ← 高さを指定
-      decoration: const BoxDecoration(
-        color: Color(0xFF55C500), // ← 背景色を指定
-        borderRadius: BorderRadius.all(
-          // ← 角丸を設定
-          Radius.circular(32),
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 12,
+        horizontal: 16,
+      ),
+      child: Container(
+        height: 180, // ← 高さを指定
+        decoration: const BoxDecoration(
+          color: Color(0xFF55C500), // ← 背景色を指定
+          borderRadius: BorderRadius.all(
+            // ← 角丸を設定
+            Radius.circular(32),
+          ),
         ),
       ),
     );
