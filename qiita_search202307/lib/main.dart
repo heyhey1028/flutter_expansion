4ff22b0d54faf588078d3d509c95f6bda75f3727
heyhey1028 <shoheiogawa1028@gmail.com>
2023-07-29 12:06:06 +0900

    Change main function to use Future and load .env file

--- qiita_search202307/lib/main.dart
+++ qiita_search202307/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:qiita_search202307/screens/search_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart'; // packageをインポート
 
-void main() {
+// main関数をFutureに変更
+Future<void> main() async {
+  await dotenv.load(fileName: '.env'); // .envファイルを読み込み
   runApp(const MainApp());
 }
 
