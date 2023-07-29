f81760ccdb61c22cc1c5d3ece3348fe45e837e4e
heyhey1028 <shoheiogawa1028@gmail.com>
2023-07-29 14:10:03 +0900

    Add searchQiita method for searching articles

--- qiita_search202307/lib/screens/search_screen.dart
+++ qiita_search202307/lib/screens/search_screen.dart
@@ -1,4 +1,10 @@
+// ignore_for_file: unused_import
+
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http; // httpという変数を通して、httpパッケージにアクセス
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:qiita_search202307/models/article.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -16,5 +22,31 @@ class _SearchScreenState extends State<SearchScreen> {
       ),
       body: Container(),
     );
+  }
+
+  Future<List<Article>> searchQiita(String keyword) async {
+    // 1. http通信に必要なデータを準備をする
+    //   - URL、クエリパラメータの設定
+    final uri = Uri.https('qiita.com', '/api/v2/items', {
+      'query': 'title:$keyword',
+      'per_page': '10',
+    });
+    //   - アクセストークンの取得
+    final String token = dotenv.env['QIITA_ACCESS_TOKEN'] ?? '';
+
+    // 2. Qiita APIにリクエストを送る
+    final http.Response res = await http.get(uri, headers: {
+      'Authorization': 'Bearer $token',
+    });
+
+    // 3. 戻り値をArticleクラスの配列に変換
+    // 4. 変換したArticleクラスの配列を返す(returnする)
+    if (res.statusCode == 200) {
+      // レスポンスをモデルクラスへ変換
+      final List<dynamic> body = jsonDecode(res.body);
+      return body.map((dynamic json) => Article.fromJson(json)).toList();
+    } else {
+      return [];
+    }
   }
 }
