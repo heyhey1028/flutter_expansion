f5d076af7219c3250d1394325cedd0de96adefa0
heyhey1028 <shoheiogawa1028@gmail.com>
2023-08-13 15:52:12 +0900

    feat: add screen skelton

---
+++ supa_chat_flutter/lib/pages/login_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Login Page'),
+      ),
+    );
+  }
+}
---
+++ supa_chat_flutter/lib/pages/top_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TopPage extends StatefulWidget {
+  const TopPage({super.key});
+
+  @override
+  State<TopPage> createState() => _TopPageState();
+}
+
+class _TopPageState extends State<TopPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Top Page'),
+      ),
+    );
+  }
+}
